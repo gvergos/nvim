fc546aaff9e80b29f6994376a1c4a439b97f53d2
gvergos <vergosgeo@gmail.com>
2025-10-04 08:15:22 +0300

    Added filters in nvim tree

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,14 +1,14 @@
 local api = require "nvim-tree.api"
 
 local function on_my_attach(bufnr)
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-  api.config.mappings.default_on_attach(bufnr)
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    api.config.mappings.default_on_attach(bufnr)
 end
 
 vim.keymap.set('n', '<C-n>', function()
-  require("nvim-tree.api").tree.toggle()
+    require("nvim-tree.api").tree.toggle()
 end, { desc = "Toggle NvimTree" })
 
 require("nvim-tree").setup({
@@ -22,7 +22,9 @@ require("nvim-tree").setup({
         group_empty = true,
     },
     filters = {
-        dotfiles = true,
+        dotfiles = false,
+        git_ignored = false,
+        custom = { ".git", "node_modules", ".cache" },
     },
     on_attach = on_my_attach,
 })
